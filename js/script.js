193c6888ff4d6e56371bab90917e1defd3b1c8fe
Dina <sharipova0308@gmail.com>
2023-05-09 21:23:53 +0500

    add providerscard map

--- js/script.js
+++ js/script.js
@@ -95,5 +95,15 @@ $('.providerscard__slider-for').slick({
     focusOnSelect: true
   });
 
+  //providerscard tabs
+  $(function() {
+  
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+      $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+    
+  });
 
     });
