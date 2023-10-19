92fb47fc73d9e7d7a315cba3d0cf9a7dc0871a6e
DinaSharipova <sharipova0308@gmail.com>
2023-10-19 11:28:42 +0500

    add adaptive menu

--- js/script.js
+++ js/script.js
@@ -60,6 +60,50 @@ function consoleBG() {
   }
   consoleBG();
 
+//dropdown menu - сужение меню при изменении ширины экрана
+function responseMenu(){
+	$('ul.dropdown-menu li.item').appendTo('ul.menu');
+	var items = $('ul.menu li.item');
+	var max_width = $('ul.menu').width() - $('ul.menu li.dd_menu').outerWidth();
+	var width = 0;
+	var hide_from = 0;
+
+	items.css({'width':'auto'});
+
+	items.each(function(index){
+		if (width + $(this).outerWidth() > max_width)
+		{
+			return false;
+		}
+		else
+		{
+			hide_from = index;
+			width += $(this).outerWidth();
+		}
+	});
+	if (hide_from < items.length - 1) {
+		items.eq(hide_from).nextAll('li.item').appendTo('ul.dropdown-menu');
+		// items.css({'width':(max_width / (hide_from)) + 'px'});
+        items.css({'width':'fit-content'});
+		$('ul.menu li.dd_menu').show();
+	}
+	else
+	{
+		$('ul.menu li.dd_menu').hide();
+	}
+}
+
+
+	$('.top_menu').on('click', '.dropdown-toggle', function () {
+		$('.dropdown-menu').toggle();
+	});
+
+	$(window).on('resize', function(){
+		responseMenu();
+	}).trigger('resize');
+
+
+
 // ПОКАЗ МОДАЛЬНЫХ ОКОН
 
     //modal login войти зарегистрироваться
