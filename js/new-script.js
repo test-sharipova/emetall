c6cded71c01777fbac3fd2144f0aa755ae0dea49
DinaSharipova <sharipova0308@gmail.com>
2024-12-14 18:37:01 +0500

    add sborn

--- js/new-script.js
+++ js/new-script.js
@@ -1,4 +1,12 @@
-$('.btn-catalog-more').on('click', function(){
-    $('.providerscard-more__menu').toggleClass('providerscard-more__menu-active');
-
+$('.btn-catalog-more').each(function(i) {
+    $(this).on('click', function(){
+    $('.providerscard-more__menu').eq(i).toggleClass('providerscard-more__menu-active');
+    });
 });
+
+//разместить заявку страница applications
+// $('.applications-order').on('click', function() {
+//     $('.overlay, .modal-place-order').fadeIn();
+//   });
+
+
