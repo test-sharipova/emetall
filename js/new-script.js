c2ce1d2ba6050465dd7421e1c013d9b7a77dcf4d
DinaSharipova <sharipova0308@gmail.com>
2024-12-14 19:15:42 +0500

    add info modal

--- js/new-script.js
+++ js/new-script.js
@@ -1,12 +1,29 @@
+//показать выпадающий список меню при клике на троеточие в таблице фильтров
 $('.btn-catalog-more').each(function(i) {
     $(this).on('click', function(){
     $('.providerscard-more__menu').eq(i).toggleClass('providerscard-more__menu-active');
     });
 });
 
-//разместить заявку страница applications
-// $('.applications-order').on('click', function() {
-//     $('.overlay, .modal-place-order').fadeIn();
-//   });
+//показать модалку с измененениями в дизайне (инфо)
+let modalShown = false; // Флаг для отслеживания, показано ли модальное окно
+    let modalClosed = false; // Флаг для отслеживания, закрыто ли модальное окно
+
+    if ($(window).width() > 768) {
+        $(window).scroll(function() {
+            if (!modalShown && !modalClosed && $(this).scrollTop() > 700) {
+                $('.overlay').fadeIn();
+                $('.modal-info').fadeIn();
+                modalShown = true; // Устанавливаем флаг в true, чтобы окно больше не показывалось
+            }
+        });
+
+        // Обработчик события для кнопки закрытия модального окна
+        $('.modal-info .close-button').click(function() {
+            $('.overlay').fadeOut();
+            $('.modal-info').fadeOut();
+            modalClosed = true; // Устанавливаем флаг в true, чтобы окно не показывалось повторно
+        });
+    }
 
 
